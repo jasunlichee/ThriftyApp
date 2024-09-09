651ff2a161b46ecef594712e9a260d17d9cfccd3
Jason Li <jason_li@umail.ucsb.edu>
2024-09-09 16:42:51 -0700

    Initial Commit

---
+++ Thrifty/ThriftyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThriftyApp.swift
+//  Thrifty
+//
+//  Created by Jason Li on 9/9/24.
+//
+
+import SwiftUI
+
+@main
+struct ThriftyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
